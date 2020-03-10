6fbd8d0dfa20612f7ae280befe1a8c8136e86d5e
Zac H <zac.haluza@gmail.com>
2020-03-10 14:02:34 -0400

    added routes

---
+++ server/routes/userRoutes.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const userController = require('../controllers/userController');
+const router = express.Router();
+
+// Create new user
+router.post('/', userController.createUser, (req, res) => {
+  return res.status(200).send(res.locals.result);
+});
+
+// User signin
+router.get('/', userController.signIn, (req, res) => {
+  return res.status(200).send(res.locals.user);
+});
+
+// Update user favorites
+router.patch('/', userController.updateFavs, (req, res) => {
+  return res.status(200).send(res.locals.result);
+});
+
+module.exports = router;
