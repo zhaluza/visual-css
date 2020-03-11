be18e086a1299a6aba8ab2355d72f827e469eac2
Zac H <zac.haluza@gmail.com>
2020-03-11 00:26:12 -0400

    started setting up Redux structure

--- client/components/SliderContainer/SliderContainer.js
+++ client/components/SliderContainer/SliderContainer.js
@@ -4,8 +4,28 @@ import './sliderContainer.scss';
 const SliderContainer = props => {
   return (
     <div className="slider-container">
-      <input type="range" min="1" max="100" value="50" className="slider" />
-      <input type="range" min="1" max="100" value="50" className="slider" />
+      <div className="slider-div">
+        <p>Position X</p>
+        <input type="range" min="-200" max="200" value="0" className="slider" />
+      </div>
+
+      <div className="slider-div">
+        <p>Position Y</p>
+        <input type="range" min="-200" max="200" value="0" className="slider" />
+      </div>
+
+      <div className="slider-div">
+        <p>Blur</p>
+        <input type="range" min="1" max="100" value="50" className="slider" />
+      </div>
+
+      <div className="slider-div">
+        <p>Spread</p>
+        <input type="range" min="1" max="100" value="50" className="slider" />
+      </div>
+
+      <label>Toggle Inset</label>
+      <input type="checkbox" />
     </div>
   );
 };
--- client/index.js
+++ client/index.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Provider } from 'react-redux';
 import App from './components/App';
+import store from './store';
 import styles from '../client/scss/application.scss';
 
-render(<App />, document.getElementById('root'));
+render(
+  <Provider>
+    <App />
+  </Provider>,
+
+  document.getElementById('root')
+);
