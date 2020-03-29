7144557008a1c9d4922ef62d953ce5832b8a0e77
Zac H <zac.haluza@gmail.com>
2020-03-29 18:04:46 -0400

    added border radius reducer

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import modeReducer from './modeReducer';
 import boxShadowReducer from './boxShadowReducer';
+import borderRadiusReducer from './borderRadiusReducer';
 import userReducer from './userReducer';
 
 const reducers = combineReducers({
   mode: modeReducer,
+  borderRadius: borderRadiusReducer,
   boxShadow: boxShadowReducer,
   userInfo: userReducer
 });
