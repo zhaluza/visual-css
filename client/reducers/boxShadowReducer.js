77ca17da06918b89ce16c11d2bccfdf119f3d963
Zac H <zac.haluza@gmail.com>
2020-03-29 17:44:41 -0400

    updated with new action names

--- client/reducers/boxShadowReducer.js
+++ client/reducers/boxShadowReducer.js
@@ -11,45 +11,45 @@ const initialState = {
 
 const boxShadowReducer = (state = initialState, action) => {
   switch (action.type) {
-    case types.HANDLE_X_POSITION:
+    case types.HANDLE_X_POSITION_BOX_SHADOW:
       const posX = action.payload;
       return {
         ...state,
         posX
       };
 
-    case types.HANDLE_Y_POSITION:
+    case types.HANDLE_Y_POSITION_BOX_SHADOW:
       const posY = action.payload;
       return {
         ...state,
         posY
       };
-    case types.HANDLE_BLUR:
+    case types.HANDLE_BLUR_BOX_SHADOW:
       const blur = action.payload;
       return {
         ...state,
         blur
       };
-    case types.HANDLE_SPREAD:
+    case types.HANDLE_SPREAD_BOX_SHADOW:
       const spread = action.payload;
       return {
         ...state,
         spread
       };
-    case types.HANDLE_OPACITY:
+    case types.HANDLE_OPACITY_BOX_SHADOW:
       const opacity = action.payload / 100;
       return {
         ...state,
         opacity
       };
-    case types.TOGGLE_INSET:
+    case types.TOGGLE_INSET_BOX_SHADOW:
       const inset = action.payload;
 
       return {
         ...state,
         inset
       };
-    case types.HANDLE_RESET:
+    case types.HANDLE_RESET_BOX_SHADOW:
       return initialState;
     default:
       return state;
