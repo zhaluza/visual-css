7ab8143f73f7b40346fc342afdc89609c3df1404
Zac H <zac.haluza@gmail.com>
2020-04-12 11:33:19 -0400

    buttons now toggle mode

--- client/components/Sidebar/Sidebar.js
+++ client/components/Sidebar/Sidebar.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import './sidebar.scss';
 
-const Sidebar = props => {
+const Sidebar = (props) => {
+  const { setBoxShadowMode, setBorderRadiusMode } = props;
   return (
     <div className="sidebar">
       <h2>Visual CSS</h2>
       <p>Choose a CSS setting and experiment.</p>
-      <button className="btn">Box Shadow</button>
+      <button className="btn" onClick={setBoxShadowMode}>
+        Box Shadow
+      </button>
+      <button className="btn" onClick={setBorderRadiusMode}>
+        Border Radius
+      </button>
     </div>
   );
 };
