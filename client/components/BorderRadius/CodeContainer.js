3fd51fae62391ea9faa451bdcc352805cb30328f
Zac H <zac.haluza@gmail.com>
2020-06-13 19:34:53 -0400

    Refactored to incorporate template component

--- client/components/BorderRadius/CodeContainer.js
+++ client/components/BorderRadius/CodeContainer.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import CodeContainerTemplate from '../templates/CodeContainerTemplate';
 
 const CodeContainer = (props) => {
   const { pixels, percent } = props;
@@ -6,32 +7,11 @@ const CodeContainer = (props) => {
   const codeStringPercent = `border-radius: ${percent}%`;
 
   const [pixOrPerc, setPixOrPcnt] = useState('pixels');
-  const [copied, setCopied] = useState(false);
-  const [message, setMessage] = useState('Copied!');
-
-  const copyCodeToClipboard = (e) => {
-    e.preventDefault();
-    const code = document.getElementById('code');
-    console.log(code.innerHTML);
-    code.select();
-    code.setSelectionRange(0, 9999); // For mobile
-    document.execCommand('copy');
-    displayCopiedMessage();
-  };
-
-  const displayCopiedMessage = () => {
-    setCopied(true);
-    setTimeout(() => {
-      setCopied(false);
-    }, 3000);
-  };
 
   return (
-    <div className="code-container">
-      <textarea
-        className="code"
-        readOnly={pixOrPerc === 'pixels' ? codeStringPixels : codeStringPercent}
-      />
+    <CodeContainerTemplate
+      valueProps={pixOrPerc === 'pixels' ? codeStringPixels : codeStringPercent}
+    >
       <div className="pixel-percent-buttons">
         <div className="btn" onClick={() => setPixOrPcnt('pixels')}>
           Display Pixels
@@ -40,14 +20,7 @@ const CodeContainer = (props) => {
           Display Percent
         </div>
       </div>
-      <div className="code-buttons">
-        <button className="btn" onClick={() => copyCodeToClipboard}>
-          Copy Code
-        </button>
-        <button className="btn">Add to Favorites</button>
-        {copied && <p>{message}</p>}
-      </div>
-    </div>
+    </CodeContainerTemplate>
   );
 };
 
