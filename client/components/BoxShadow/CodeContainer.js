64121b2a785393a79b52b4b5356cca25ab3b02af
Zac H <zac.haluza@gmail.com>
2020-06-13 15:02:31 -0400

    Refactored to return template component

--- client/components/BoxShadow/CodeContainer.js
+++ client/components/BoxShadow/CodeContainer.js
@@ -1,43 +1,12 @@
-import React, { useState } from 'react';
-
-// TODO: Get copy button to work
-
+import React from 'react';
+import CodeContainerTemplate from '../templates/CodeContainerTemplate';
 const CodeContainer = (props) => {
   const { posX, posY, blur, spread, opacity, inset } = props;
   const codeString = `box-shadow: ${posX}px ${posY}px ${blur}px ${spread}px rgba(0, 0, 0, ${opacity})`;
   const insetCodeString = `box-shadow: inset ${posX}px ${posY}px ${blur}px ${spread}px rgba(0, 0, 0, ${opacity})`;
 
-  const [copied, setCopied] = useState(false);
-  const [message, setMessage] = useState('Copied!');
-
-  const copyCodeToClipboard = (e) => {
-    e.preventDefault();
-    const code = document.getElementById('code');
-    console.log(code.innerHTML);
-    code.select();
-    code.setSelectionRange(0, 9999); // For mobile
-    document.execCommand('copy');
-    displayCopiedMessage();
-  };
-
-  const displayCopiedMessage = () => {
-    setCopied(true);
-    setTimeout(() => {
-      setCopied(false);
-    }, 3000);
-  };
-
   return (
-    <div className="code-container">
-      <textarea className="code" value={inset ? insetCodeString : codeString} />
-      <div className="code-buttons">
-        <button className="btn" onClick={copyCodeToClipboard}>
-          Copy Code
-        </button>
-        <button className="btn">Add to Favorites</button>
-        {copied && <p>{message}</p>}
-      </div>
-    </div>
+    <CodeContainerTemplate valueProps={inset ? insetCodeString : codeString} />
   );
 };
 
