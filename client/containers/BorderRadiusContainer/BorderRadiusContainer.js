f1bd512e5cdebc3bdc23bb7e649c5b7b20baa3fb
Zac H <zac.haluza@gmail.com>
2020-06-13 15:08:52 -0400

    Initial container

---
+++ client/containers/BorderRadiusContainer/BorderRadiusContainer.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import * as actions from '../../actions/actions';
+import CodeContainer from '../../components/BorderRadius/CodeContainer';
+import CSSImage from '../../components/BorderRadius/CSSImage';
+import SliderContainer from '../../components/BorderRadius/SliderContainer';
+
+// TODO: implement this for percentages too
+
+const mapStateToProps = (state) => ({
+  pixels: state.borderRadius.pixels,
+  percent: state.borderRadius.percent,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  handlePixels: (event) =>
+    dispatch(actions.handlePixelsBorderRadius(event.target.value)),
+  handlePercents: (event) =>
+    dispatch(actions.handlePercentsBorderRadius(event.target.value)),
+  handleReset: () => dispatch(actions.handleResetBorderRadius()),
+});
+
+const BorderRadiusContainer = (props) => {
+  const { pixels, percent, handlePixels, handlePercents, handleReset } = props;
+  return (
+    <div className="css-mode-container">
+      <CSSImage pixels={pixels} percent={percent} />
+      <div className="container-bottom">
+        <SliderContainer
+          pixels={pixels}
+          percent={percent}
+          handlePixels={handlePixels}
+          handlePercents={handlePercents}
+        />
+        <CodeContainer pixels={pixels} percent={percent} />
+      </div>
+    </div>
+  );
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(BorderRadiusContainer);
