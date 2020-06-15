10cb02cc8e85feb488de1d1867324b5cf82c1fac
Zac H <zac.haluza@gmail.com>
2020-06-15 11:27:56 -0400

    Replaced 2D template w/ 3D

--- client/components/CSSModes/Rotate3D/CSSImage.js
+++ client/components/CSSModes/Rotate3D/CSSImage.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import CSSImageTemplate from '../../templates/CSSImageTemplate';
+import CSS3DImageTemplate from '../../templates/CSS3DImageTemplate';
 
 const CSSImage = (props) => {
   const { x, y, z, angle } = props;
 
   const rotate3DStyle = {
     transform: `rotate3d(${x}, ${y}, ${z}, ${angle}deg)`,
-    perspective: '800px',
+    // perspective: '800px',
   };
-  return <CSSImageTemplate styleProps={rotate3DStyle} />;
+  return <CSS3DImageTemplate styleProps={rotate3DStyle} />;
 };
 
 export default CSSImage;
