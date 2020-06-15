1e168157edd5a892f055d477ed20a06386b20beb
Zac H <zac.haluza@gmail.com>
2020-06-14 23:05:39 -0400

    added 3d reducer

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -2,13 +2,15 @@ import { combineReducers } from 'redux';
 import modeReducer from './modeReducer';
 import boxShadowReducer from './boxShadowReducer';
 import borderRadiusReducer from './borderRadiusReducer';
+import rotate3DReducer from './rotate3DReducer';
 import userReducer from './userReducer';
 
 const reducers = combineReducers({
   mode: modeReducer,
   borderRadius: borderRadiusReducer,
   boxShadow: boxShadowReducer,
-  userInfo: userReducer
+  rotate3D: rotate3DReducer,
+  userInfo: userReducer,
 });
 
 export default reducers;
