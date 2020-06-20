52a26c9766d42715bec30131d63909a72415c3d6
Zac H <zac.haluza@gmail.com>
2020-06-19 23:25:57 -0400

    changed App.js path

--- client/index.js
+++ client/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import App from './components/App';
+import App from './App';
 import store from './store';
 import styles from '../client/scss/application.scss';
 
