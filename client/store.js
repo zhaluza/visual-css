5e24003095ff5367b68ee414bb112c65252211b0
Zac H <zac.haluza@gmail.com>
2020-06-19 23:26:05 -0400

    removed thunk logic

--- client/store.js
+++ client/store.js
@@ -1,11 +1,7 @@
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
+import { createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import reducers from './reducers/index';
 
-const store = createStore(
-  reducers,
-  composeWithDevTools(applyMiddleware(thunk))
-);
+const store = createStore(reducers, composeWithDevTools());
 
 export default store;
