0ab488caf7e0aa1873a32c598b7ca98f0420eaab
Zac H <zac.haluza@gmail.com>
2020-06-19 23:27:21 -0400

    removed user reducer

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -3,14 +3,12 @@ import modeReducer from './modeReducer';
 import boxShadowReducer from './boxShadowReducer';
 import borderRadiusReducer from './borderRadiusReducer';
 import rotate3DReducer from './rotate3DReducer';
-import userReducer from './userReducer';
 
 const reducers = combineReducers({
   mode: modeReducer,
   borderRadius: borderRadiusReducer,
   boxShadow: boxShadowReducer,
   rotate3D: rotate3DReducer,
-  userInfo: userReducer,
 });
 
 export default reducers;
