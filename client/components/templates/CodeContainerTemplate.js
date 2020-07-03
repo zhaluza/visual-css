25d367a1ebe392f308cfd27c3ea6f37956c0d1da
Zac H <zac.haluza@gmail.com>
2020-07-03 10:34:18 -0400

    Now able to copy code snippets to clipboard

--- client/components/templates/CodeContainerTemplate.js
+++ client/components/templates/CodeContainerTemplate.js
@@ -1,24 +1,37 @@
-import React, { useState } from 'react';
-import {
-  copyCodeToClipboard,
-  displayCopiedMessage,
-} from '../../functions/copyCode';
-
-// TODO: Get copy button to work
+import React, { useState, useRef } from 'react';
+import { copyCodeToClipboard } from '../../functions/copyCode';
 
 const CodeContainerTemplate = (props) => {
   const [copied, setCopied] = useState(false);
   const [message, setMessage] = useState('Copied!');
+  const textAreaRef = useRef(null);
 
+  function copyCodeToClipBoard(e) {
+    console.log('copying');
+    console.log(e);
+    textAreaRef.current.select();
+    document.execCommand('copy');
+    e.target.focus();
+    displayCopiedMessage();
+  }
+
+  function displayCopiedMessage() {
+    setCopied(true);
+    setTimeout(() => {
+      setCopied(false);
+    }, 3000);
+  }
   return (
     <div className="code-container">
-      <textarea className="code" value={props.valueProps} />
+      <textarea className="code" value={props.valueProps} ref={textAreaRef} />
       <div className="code-buttons">
         {props.children}
-        <button className="btn" onClick={copyCodeToClipboard}>
-          Copy Code
-        </button>
-        <button className="btn">Add to Favorites</button>
+        {document.queryCommandSupported('copy') && (
+          <button className="btn copy-btn" onClick={copyCodeToClipBoard}>
+            Copy Code
+          </button>
+        )}
+
         {copied && <p>{message}</p>}
       </div>
     </div>
