b02cb698a5eb9cb31b9227e7fef06ccc1c572f6e
Zac H <zac.haluza@gmail.com>
2020-07-06 16:22:06 -0400

    toggle button color when clicked

--- client/components/Sidebar/Sidebar.js
+++ client/components/Sidebar/Sidebar.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import './sidebar.scss';
 
-const Sidebar = (props) => {
-  const { setBoxShadowMode, setBorderRadiusMode, setRotate3DMode } = props;
+const Sidebar = ({ setBoxShadowMode, setBorderRadiusMode, setRotate3DMode, mode }) => {
   return (
     <div className="sidebar">
       <h2>Visual CSS</h2>
       <p>Choose a CSS setting and experiment.</p>
-      <button className="btn" onClick={setBoxShadowMode}>
+      <button className={`btn ${mode === 'box-shadow' ? 'active' : ''}`} onClick={setBoxShadowMode}>
         Box Shadow
       </button>
-      <button className="btn" onClick={setBorderRadiusMode}>
+      <button
+        className={`btn ${mode === 'border-radius' ? 'active' : ''}`}
+        onClick={setBorderRadiusMode}
+      >
         Border Radius
       </button>
-      <button className="btn" onClick={setRotate3DMode}>
+      <button className={`btn ${mode === 'rotate-3d' ? 'active' : ''}`} onClick={setRotate3DMode}>
         Rotate 3D
       </button>
     </div>
