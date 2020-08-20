206ff941de773d22e280fc30532ee31e2ee9b637
Zac H <zac.haluza@gmail.com>
2020-08-20 16:19:00 -0400

    remove backend functionality (focus on frontend)

--- client/components/Sidebar/Sidebar.js
+++ client/components/Sidebar/Sidebar.js
@@ -13,11 +13,6 @@ const Sidebar = ({
     <div className={`sidebar ${isMobile && 'sidebar-mobile'} ${!menuOpen && 'sidebar-hidden'}`}>
       <h2>Visual CSS</h2>
       <p>Choose a CSS setting and experiment.</p>
-      <button>
-        <a href="https://github.com/login/oauth/authorize?client_id=b5c4eac00ad59b774b91">
-          Log in with Github
-        </a>
-      </button>
       <button className={`btn ${mode === 'box-shadow' ? 'active' : ''}`} onClick={setBoxShadowMode}>
         Box Shadow
       </button>
--- server/controllers/snippetController.js
+++
@@ -1,9 +0,0 @@
-const User = require('../models/userModel');
-
-const authController = {};
-
-// TODO: fetch all of user's code snippets
-
-// TODO: add code snippet
-
-// TODO: delete code snippet
