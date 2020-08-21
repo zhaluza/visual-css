6a218ca3f59180f25da17cc9e0ea92a640bf8621
Zac H <zac.haluza@gmail.com>
2020-08-21 16:24:01 -0400

    fix inline styling error

--- client/components/CSSModes/BoxShadow/CSSImage.js
+++ client/components/CSSModes/BoxShadow/CSSImage.js
@@ -2,18 +2,17 @@ import React from 'react';
 import CSSImageTemplate from '../../templates/CSSImageTemplate';
 
 const CSSImage = (props) => {
-  const { posX, posY, spread, blur, inset, opacity } = props;
+  const { posX, posY, spread, blur, inset, opacity, boxColor } = props;
 
   const squareStyle = {
     boxShadow: `${posX}px ${posY}px ${blur}px ${spread}px rgba(0, 0, 0, ${opacity})`,
+    backgroundColor: `${boxColor}`,
   };
 
   const squareStyleInset = {
     boxShadow: `inset ${posX}px ${posY}px ${blur}px ${spread}px rgba(0, 0, 0, ${opacity})`,
   };
-  return (
-    <CSSImageTemplate styleProps={inset ? squareStyleInset : squareStyle} />
-  );
+  return <CSSImageTemplate styleProps={inset ? squareStyleInset : squareStyle} />;
 };
 
 export default CSSImage;
