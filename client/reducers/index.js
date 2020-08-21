1e6fae6c0eaba0c9fb21fb0e92deba4e42b80fb9
Zac H <zac.haluza@gmail.com>
2020-08-21 16:56:24 -0400

    add boxColor reducer

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -1,11 +1,13 @@
 import { combineReducers } from 'redux';
 import modeReducer from './modeReducer';
+import boxColorReducer from './boxColorReducer';
 import boxShadowReducer from './boxShadowReducer';
 import borderRadiusReducer from './borderRadiusReducer';
 import rotate3DReducer from './rotate3DReducer';
 
 const reducers = combineReducers({
   mode: modeReducer,
+  boxColor: boxColorReducer,
   borderRadius: borderRadiusReducer,
   boxShadow: boxShadowReducer,
   rotate3D: rotate3DReducer,
