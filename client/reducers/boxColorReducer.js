a761ec1b25772dd28da563b9bfa525045d4370f9
Zac H <zac.haluza@gmail.com>
2020-08-21 22:00:28 -0400

    reformat color to rgba

--- client/reducers/boxColorReducer.js
+++ client/reducers/boxColorReducer.js
@@ -1,8 +1,7 @@
 import * as types from '../constants/actionTypes';
 
-const defaultColor = '#ff6b6b';
 const initialState = {
-  backgroundColor: defaultColor,
+  boxColor: { r: 255, g: 107, b: 107, a: 1 },
 };
 
 const colorReducer = (state = initialState, action) => {
@@ -10,11 +9,11 @@ const colorReducer = (state = initialState, action) => {
     case types.HANDLE_BOX_COLOR:
       const newColor = action.payload;
       return {
-        backgroundColor: newColor,
+        boxColor: newColor,
       };
     case types.RESET_BOX_COLOR:
       return {
-        backgroundColor: defaultColor,
+        boxColor: { ...initialState.boxColor },
       };
     default:
       return state;
