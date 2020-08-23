17836cae5eb672426d81d5c4f5572143eaef0fb3
Zac H <zac.haluza@gmail.com>
2020-08-22 22:20:07 -0400

    implement basic pixel/percent toggle UI

--- client/components/CSSModes/BorderRadius/SliderContainer.js
+++ client/components/CSSModes/BorderRadius/SliderContainer.js
@@ -1,33 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import SliderContainerTemplate from '../../templates/SliderContainerTemplate';
 
 const SliderContainer = (props) => {
   const { pixels, percent, handlePixels, handlePercents, handleReset } = props;
+  const [togglePixels, setTogglePixels] = useState(true);
 
   return (
     <SliderContainerTemplate handleReset={handleReset}>
-      <div className="slider-div">
-        <p>Border Radius (pixels): {pixels}px</p>
-        <input
-          type="range"
-          min="0"
-          max="100"
-          value={pixels}
-          onChange={handlePixels}
-          className="slider"
-        />
-      </div>
-
-      <div className="slider-div">
-        <p>Border Radius (percent): {percent}%</p>
-        <input
-          type="range"
-          min="0"
-          max="100"
-          value={percent}
-          onChange={handlePercents}
-          className="slider"
-        />
+      {togglePixels ? (
+        <div className="slider-div">
+          <p>Border Radius: {pixels}px</p>
+          <input
+            type="range"
+            min="0"
+            max="100"
+            value={pixels}
+            onChange={handlePixels}
+            className="slider"
+          />
+        </div>
+      ) : (
+        <div className="slider-div">
+          <p>Border Radius: {percent}%</p>
+          <input
+            type="range"
+            min="0"
+            max="100"
+            value={percent}
+            onChange={handlePercents}
+            className="slider"
+          />
+        </div>
+      )}
+      <div className="toggle-container" onClick={() => setTogglePixels((prevState) => !prevState)}>
+        <button className={`toggle-btn ${togglePixels ? '' : 'toggle-left'}`}>
+          {togglePixels ? 'Pixels' : 'Percent'}
+        </button>
       </div>
     </SliderContainerTemplate>
   );
