24093febb9258b09713aecce9fca027f334d8282
Zac H <zac.haluza@gmail.com>
2020-08-22 22:32:31 -0400

    toggle CSS between px and %

--- client/components/CSSModes/BorderRadius/CodeContainer.js
+++ client/components/CSSModes/BorderRadius/CodeContainer.js
@@ -1,26 +1,14 @@
 import React, { useState } from 'react';
 import CodeContainerTemplate from '../../templates/CodeContainerTemplate';
 
-const CodeContainer = (props) => {
-  const { pixels, percent } = props;
+const CodeContainer = ({ pixels, percent, togglePixels }) => {
   const codeStringPixels = `border-radius: ${pixels}px`;
   const codeStringPercent = `border-radius: ${percent}%`;
 
-  const [pixOrPerc, setPixOrPcnt] = useState('pixels');
-
   return (
     <CodeContainerTemplate
-      valueProps={pixOrPerc === 'pixels' ? codeStringPixels : codeStringPercent}
-    >
-      <div className="pixel-percent-buttons">
-        <div className="btn" onClick={() => setPixOrPcnt('pixels')}>
-          Display Pixels
-        </div>
-        <div className="btn" onClick={() => setPixOrPcnt('percent')}>
-          Display Percent
-        </div>
-      </div>
-    </CodeContainerTemplate>
+      valueProps={togglePixels ? codeStringPixels : codeStringPercent}
+    ></CodeContainerTemplate>
   );
 };
 
